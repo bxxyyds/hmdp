ca35dc379553624f82243d5190aaecbe253b0b11
女巫 <3326553458@qq.com>
2026-08-20 15:17:15 +0800

    feat: name

--- src/main/java/com/hmdp/mapper/BlogCommentsMapper.java
+++ src/main/java/com/hmdp/mapper/BlogCommentsMapper.java
@@ -8,7 +8,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  *  Mapper 接口
  * </p>
  *
- * @author 虎哥
+ * @author 我是孙斌
  * @since 2021-12-22
  */
 public interface BlogCommentsMapper extends BaseMapper<BlogComments> {
